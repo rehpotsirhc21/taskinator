71b6376edbf82ab8cd22ea2d3ce67064bf7c775d
Christopher Langner <langnerc@icloud.com>
2022-03-20 11:18:41 -0500

    cleared browswer deault behavior

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,14 @@
 // create variables for the button and the task list
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 // function to add a new task item
 var createTaskHandler = function () {
+    event.preventDefault();
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "this is a new task";
   tasksToDoEl.appendChild(listItemEl);
+ 
 };
 // listen for the click
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
