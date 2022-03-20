bd50808c64bbb69bafe161cd255633320559a11f
Christopher Langner <langnerc@icloud.com>
2022-03-20 11:48:54 -0500

    added js to add h3 objects to the task list with the title and type

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,12 +3,27 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 // function to add a new task item
 var createTaskHandler = function () {
-    event.preventDefault();
+  // prevent refresh
+  event.preventDefault();
+  //   grab the value of the task name and type from the HTML form
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  //   create the list item using the input and var above
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "this is a new task";
+  //   create div to hold task info and add to the list item
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+//   add new task to the html document
+  taskInfoEl.innerHTML =
+    "<h3 class='task-name'>" +
+    taskNameInput +
+    "</h3><span class='task-type'>" +
+    taskTypeInput +
+    "</span>";
+  listItemEl.appendChild(taskInfoEl);
+// add entire list item to the list
   tasksToDoEl.appendChild(listItemEl);
- 
 };
 // listen for the click
 formEl.addEventListener("submit", createTaskHandler);
